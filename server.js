d378f4371ef44282905489d2244037464c7beab9
Leia <87383466+klynmc@users.noreply.github.com>
2021-10-20 14:16:12 -0500

    add 404 error

--- server.js
+++ server.js
@@ -44,14 +44,20 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    if (result) {
+        res.json(result);
+      } else {
+        res.send(404);
     }
-    res.json(results);
+}
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+      res.json(result);
 });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-  });
+});
